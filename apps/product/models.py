6d094d994840d543a429177f28369d3101c9030c
tuanminh2002 <tenlaminh2k2@gmail.com>
2022-11-02 10:43:26 +0700

    update app product

---
+++ apps/product/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=12)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
